a48d3d70ba0a0888e050e788979ee2be76acb482
irhamswandaru <mirhamswandaru@gmail.com>
2024-08-23 16:51:44 +0700

    first commit

---
+++ animate.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const newtonCard = document.querySelector('.card.animate-slide-in-right');
+    newtonCard.classList.add('animate-slide-in-right');
+
+    const hukumNewtonCard = document.querySelector('.card.animate-slide-in-left');
+    hukumNewtonCard.classList.add('animate-slide-in-left');
+
+    const newtonImage = document.querySelector('.img-fluid.animate-scale-up');
+    newtonImage.classList.add('animate-scale-up');
+});
